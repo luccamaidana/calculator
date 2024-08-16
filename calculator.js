af17bfabc72df74a88ad0352d4ba0c14f526d55c
luccamaidana <99457796+luccamaidana@users.noreply.github.com>
2024-08-16 00:03:05 -0300

    Add button box and display numbers

--- calculator.js
+++ calculator.js
@@ -17,9 +17,9 @@ const div = (n1,n2)=>{
         return n1 / n2
 }
 
-let n1 = Number
+/* let n1 = Number
 let n2 = Number
-let oper = String
+let oper = String */
 
 const operate = (n1,n2,oper)=>{
     switch (oper) {
@@ -38,8 +38,29 @@ const operate = (n1,n2,oper)=>{
     }
 }
 
-console.log(add(5,5))
+const display = document.getElementById('display')
+const buttons = document.querySelectorAll('#buttonBox button')
+
+buttons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        if(button.id === 'clear')
+            display.value = ''
+        else if(button.value >= 0 && button.value <= 9)
+            display.value += button.value
+        else{
+            let n1 = button.value
+            let oper = button.value
+            let n2 = button.value
+            /* display.value += button.value */
+        }
+
+            
+    })
+})
+
+/* console.log(add(5,5))
 console.log(sub(15,10))
 console.log(mult(10,10))
-console.log(div(10,5))
+console.log(div(10,5)) */
 
+console.log(operate(1,1,'+'));
