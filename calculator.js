0b08095f52ca76a3c45504be1e4e46cf36988379
luccamaidana <99457796+luccamaidana@users.noreply.github.com>
2025-06-06 17:32:29 -0300

    Disable zoom by double touch

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  const now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
+
+
 const display = document.getElementById('display')
 const buttons = document.querySelectorAll('#buttonBox button')
 
